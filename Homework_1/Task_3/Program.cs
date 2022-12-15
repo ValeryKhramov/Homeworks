3ff5e60c31c5a5b7dcfbac2f7290a71c370ddf69
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-15 23:26:57 +0300

    Added a solution to the third task.

--- Homework_1/Task_3/Program.cs
+++ Homework_1/Task_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Явлется ли число четным?");
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine());
+
+if (number % 2 == 0)
+{
+    WriteLine($"Число {number} - четное.");
+}
+else
+{
+    WriteLine($"Число {number} - нечетное.");
+}
