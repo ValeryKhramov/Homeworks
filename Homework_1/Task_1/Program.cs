e65211f7c6773e2900edda432043ac25153a4895
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-15 23:09:48 +0300

    Added a solution to the first task.

--- Homework_1/Task_1/Program.cs
+++ Homework_1/Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Сейчас мы найдем максимальное и минимальное значение из двух цифр.");
+WriteLine("Введите первое число: ");
+int number_1 = int.Parse(ReadLine()!);
+
+WriteLine("Введите второе число: ");
+int number_2 = int.Parse(ReadLine());
+
+if (number_1 > number_2)
+{
+    WriteLine($"Max = {number_1}");
+    WriteLine($"Min = {number_2}");
+}
+else
+{
+    WriteLine($"Max = {number_2}");
+    WriteLine($"Min = {number_1}");
+}
+
+
