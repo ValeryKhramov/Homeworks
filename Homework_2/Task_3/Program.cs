9b8d459f7019bff444788598abce8b62fa7b4b67
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-19 23:19:34 +0300

    Added a solution to the Task_3 Homework_2

---
+++ Homework_2/Task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите цифру дня недели: ");
+int number = int.Parse(ReadLine());
+WriteLine(number >= 1 && number <= 7? (number == 6 || number == 7? "Да": "Нет"):"Ошибка ввода.");
