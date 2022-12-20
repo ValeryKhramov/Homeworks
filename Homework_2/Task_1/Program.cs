b9219752d86b3bcf3e4f132bedfd178ac5070e02
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-20 22:53:43 +0300

    Fixed bug in Task_1 Homework_2

--- Homework_2/Task_1/Program.cs
+++ Homework_2/Task_1/Program.cs
@@ -4,4 +4,4 @@ Clear();
 WriteLine("Введите трехзначное число: ");
 int number = int.Parse(ReadLine());
 
-WriteLine(number % 100 / 10);
+WriteLine(number >= 100 && number <= 999? number % 100 / 10 : "Ошибка ввода.");
--- Homework_2/Task_3/Program.cs
+++ Homework_2/Task_3/Program.cs
@@ -3,4 +3,4 @@ Clear();
 
 WriteLine("Введите цифру дня недели: ");
 int number = int.Parse(ReadLine());
-WriteLine(number >= 1 && number <= 7? (number == 6 || number == 7? "Да": "Нет"):"Ошибка ввода.");
+WriteLine(number >= 1 && number <= 7? (number == 6 || number == 7? "Да": "Нет") :"Ошибка ввода.");
