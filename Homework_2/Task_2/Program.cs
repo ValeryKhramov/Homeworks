8f95dd57519a3b3402f6233175c955be35715834
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-20 22:49:21 +0300

    Added a solution to the Task_2 Homework_2

--- Homework_2/Task_2/Program.cs
+++ Homework_2/Task_2/Program.cs
@@ -4,4 +4,15 @@ Clear();
 WriteLine("Введите число: ");
 int number = int.Parse(ReadLine());
 
-WriteLine(number <= 100? "Третьей цифры нет" : );
+if (number >= 100)
+{
+    while (number >= 999)
+    {
+        number = number / 10;
+    }
+    WriteLine(number % 10);
+}
+else
+{
+    WriteLine("Третьей цифры нет.");
+}
