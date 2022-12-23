5880d8687b9ee3c229e799a7208048d4cc28fa9a
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-23 21:19:06 +0300

    Added a solution to the Task_1 Homework_3

--- Homework_3/Task_1/Program.cs
+++ Homework_3/Task_1/Program.cs
@@ -2,3 +2,25 @@
 
 using static System.Console;
 Clear();
+
+Write("Input five-digit number: ");
+if(!int.TryParse(ReadLine(), out int number))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
+
+if (number < 10000 || number > 99999)
+{
+    WriteLine("Error.Input five-digit nimber.");
+    return;
+}
+
+if (number % 10 == number / 10000 && number % 100 / 10 == number / 1000 % 10 )
+{
+    WriteLine("Yes");
+}
+else
+{
+    WriteLine("No");
+}
