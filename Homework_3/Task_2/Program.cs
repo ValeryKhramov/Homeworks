eeb661713c69e65f426f86dab4286e3fa7fe3ed6
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-23 21:40:24 +0300

    Added a solution to the Task_2 Homework_3

--- Homework_3/Task_2/Program.cs
+++ Homework_3/Task_2/Program.cs
@@ -3,3 +3,22 @@
 
 using static System.Console;
 Clear();
+
+ WriteLine("Input coordinates the first point (x,y,z): ");
+ if(!int.TryParse(ReadLine(), out int x) || !int.TryParse(ReadLine(), out int y) || !int.TryParse(ReadLine(), out int z))
+ {
+    WriteLine("Error. Input integer, not string");
+    return;
+ }
+
+  WriteLine("Input coordinates the second point (x,y,z): ");
+ if(!int.TryParse(ReadLine(), out int x1) || !int.TryParse(ReadLine(), out int y1) || !int.TryParse(ReadLine(), out int z1))
+ {
+    WriteLine("Error. Input integer, not string");
+    return;
+ }
+
+ double Distance = Math.Sqrt(Math.Pow((x1 - x), 2) + Math.Pow((y1 - y), 2) + Math.Pow((z1 - z), 2));
+
+ WriteLine($"Distance between two points equal to {Distance}");
+
