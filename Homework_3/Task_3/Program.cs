d2dfe8a32ef8d0d9b33d7bfdb00efa48cf1c2134
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-23 21:52:40 +0300

    Added a solution to the Task_3 Homework_3

--- Homework_3/Task_3/Program.cs
+++ Homework_3/Task_3/Program.cs
@@ -2,3 +2,29 @@
 
 using static System.Console;
 Clear();
+
+Write("Input any integer: ");
+if(!int.TryParse(ReadLine(), out int N))
+{
+    WriteLine("Error. Input integer, not string.");
+    return;
+}
+int count = 1;
+
+if (N > 0)
+{
+    while (count < N + 1)
+    {
+        Write($"{Math.Pow(count, 3)} ");
+        count++;
+    }
+}
+else
+{
+    count = -count;
+  while (count > N - 1)
+    {
+        Write($"{Math.Pow(count, 3)} ");
+        count--;
+    }  
+}
