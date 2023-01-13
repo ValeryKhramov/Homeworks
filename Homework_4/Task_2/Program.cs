69d42c01bfe24df22d137c6351f5257cda6fa7f7
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-14 00:43:31 +0300

    Added solution the Task_2

--- Homework_4/Task_2/Program.cs
+++ Homework_4/Task_2/Program.cs
@@ -1,2 +1,31 @@
-﻿using static System.Console;
+﻿// Задача 27: Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+using static System.Console;
 Clear();
+
+Write("Input integer number: ");
+if(!int.TryParse(ReadLine(), out int number))
+{
+    WriteLine("Error. Input integer number.");
+    return;
+}
+
+int outcome = numSum(number);
+WriteLine($"The sum of the digit = {outcome}");
+
+
+int numSum(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
