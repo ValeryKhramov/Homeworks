de10dee8a0df02e0299e362f41d40a0c80d681b8
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-22 14:32:08 +0300

    Added a solution Task_1 and Task_2 from Homework_6

--- Homework_6/Task_1/Program.cs
+++ Homework_6/Task_1/Program.cs
@@ -1,2 +1,46 @@
-﻿using static System.Console;
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+using static System.Console;
 Clear();
+Write("Enter some integer numbers separated by space: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+int[] elementsInt = convertArray(parameters);
+PrintArray(elementsInt);
+int resultPositiveNum = findPositiveCount(elementsInt);
+WriteLine($"Sum positive number in array = {resultPositiveNum}.");
+
+
+
+int[] convertArray(string[] array)
+{
+    int[] resultArray = new int[array.Length];
+    for(int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = Convert.ToInt32(array[i]);
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] arr)
+{
+    Write($"[ ");
+     for(int i = 0; i < arr.Length - 1; i++)
+    {
+        Write($"{arr[i]} ");
+    }
+    WriteLine($"{arr[arr.Length - 1]} ]");
+}
+
+int findPositiveCount(int[] collection)
+{
+    int count = 0;
+    foreach(int dig in collection)
+    {
+        if (dig > 0) count += 1;
+    }
+    return count;
+}
--- Homework_6/Task_2/Program.cs
+++ Homework_6/Task_2/Program.cs
@@ -1,2 +1,34 @@
-﻿using static System.Console;
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
 Clear();
+
+Write("Enter value B and K for first direct by spase: ");
+Direct firstDirect = GetDirectFromUser(ReadLine()!);
+Write("Enter value B and K for second direct by spase: ");
+Direct secondDirect = GetDirectFromUser(ReadLine()!);
+
+double XPoint = (secondDirect.B - firstDirect.B)/(firstDirect.K - secondDirect.K);
+double YPoint = (firstDirect.K * XPoint + firstDirect.B); 
+
+WriteLine($"The intersection point of the lines has the following coordinates ({XPoint}:{YPoint})");
+
+
+Direct GetDirectFromUser(string inStr)
+{
+    Direct result = new Direct();
+    string[] parameters = inStr.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    result.B = Convert.ToDouble(parameters[0]);
+    result.K = Convert.ToDouble(parameters[1]);
+    return result;
+}
+
+
+struct Direct
+{
+    public double B;
+    public double K;
+}
+
