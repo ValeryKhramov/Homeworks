929579cded749547907f84e04a16aa09c3902b13
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-30 14:25:39 +0300

    Added a solution Task_4 Homework_8

--- Homework_8/Task_4/Program.cs
+++ Homework_8/Task_4/Program.cs
@@ -8,3 +8,56 @@
 
 using static System.Console;
 Clear();
+Write("Enter count rows, columns and block in matrix by space: ");
+int[] parameters = Array.ConvertAll((ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries)), int.Parse);
+
+int[,,] elements = CreateMatrix(parameters[0],parameters[1],parameters[2]);
+PrintMatrix(elements);
+
+
+int[,,] CreateMatrix(int rows, int columns, int block)
+{
+    int total = 0;
+    int count = 0;
+    int[,,] resultMatrix = new int[rows,columns,block];
+    Random rnd = new Random();
+     while(total < resultMatrix.GetLength(0) * resultMatrix.GetLength(1) * resultMatrix.GetLength(2) )
+    {
+        for (int i = 10; i < 100; i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(0) ; j++)
+            {
+                for (int k = 0; k < resultMatrix.GetLength(1); k++)
+                {
+                    for (int n = 0; n < resultMatrix.GetLength(2); n++)
+                    {
+                        if (resultMatrix[j,k,n] == 0) resultMatrix[j,k,n] = rnd.Next(10,100);
+                        if (resultMatrix[j,k,n] == i) count += 1;
+                        if (count >= 2) resultMatrix[j,k,n] = rnd.Next(10,100);
+                    }
+                }
+            }
+            count = 0;
+        }
+        total++;
+   
+    }
+    
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < inMatrix.GetLength(2); k++)
+            {
+                Write($"{inMatrix[i,j,k],5}{(i,j,k)}");
+            }
+            WriteLine();
+        }
+        
+    }
+}
