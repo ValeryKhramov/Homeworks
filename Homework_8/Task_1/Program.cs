0a374fe51d31ed08714f6d55c7f4dce275ca70a3
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-30 09:44:58 +0300

    Added a solution Task_1 Homework_8

--- Homework_8/Task_1/Program.cs
+++ Homework_8/Task_1/Program.cs
@@ -11,3 +11,56 @@
 
 using static System.Console;
 Clear();
+Write("Enter count rows and column in nartix by space: ");
+int[] parameters = Array.ConvertAll((ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries)), int.Parse);
+
+int[,] elements = CreateMatrix(parameters[0],parameters[1]);
+PrintMatrix(elements);
+WriteLine();
+SortRowsMarix(elements);
+PrintMatrix(elements);
+
+
+int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows,columns];
+    Random rnd = new Random();
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i,j] = rnd.Next(-10,10);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j], 5}");
+        }
+        WriteLine();
+    }
+}
+
+void SortRowsMarix(int[,] inMatrix)
+{   
+    int temp = int.MinValue;
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < inMatrix.GetLength(1); k++)
+            {
+                if(inMatrix[i,j] <= inMatrix[i,k]) continue;
+                temp = inMatrix[i,j];
+                inMatrix[i,j] = inMatrix[i,k];
+                inMatrix[i,k] = temp;
+            }
+        }
+    }
+}
