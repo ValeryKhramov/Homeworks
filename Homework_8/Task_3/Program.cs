c58aefff49f85ebc99a70a4c10e497071965aa3e
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-30 10:55:27 +0300

    Added a solution Task_3 Homework_8

--- Homework_8/Task_3/Program.cs
+++ Homework_8/Task_3/Program.cs
@@ -8,3 +8,72 @@
 
 using static System.Console;
 Clear();
+Write("Enter count rows and column in first matrix by space: ");
+int[] parametersOne = Array.ConvertAll((ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries)), int.Parse);
+
+Write("Enter count rows and column in second matrix by space: ");
+int[] parametersTwo = Array.ConvertAll((ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries)), int.Parse);
+
+int[,] elements = CreateMatrix(parametersOne[0],parametersOne[1]);
+PrintMatrix(elements);
+WriteLine();
+
+int[,] collecion = CreateMatrix(parametersTwo[0],parametersTwo[1]);
+PrintMatrix(collecion);
+WriteLine();
+
+if(elements.GetLength(1) == collecion.GetLength(0))
+{
+int[,] productMatrix = MatrixMultiplication(elements, collecion);
+PrintMatrix(productMatrix);
+}
+else WriteLine("The product of the given matrices is impossible.");
+
+
+int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows,columns];
+    Random rnd = new Random();
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i,j] = rnd.Next(-10,10);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j], 5}");
+        }
+        WriteLine();
+    }
+}   
+
+int[,] MatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
+{
+    // if (firstMatrix.GetLength(1) != secondMatrix.GetLength(0)) throw new Exception("The product of the given matrices is impossible.);
+    // {
+    //     WriteLine("The product of the given matrices is impossible.");
+        
+    // }
+    int[,] resultMatrix = new int[firstMatrix.GetLength(0),secondMatrix.GetLength(1)];
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < secondMatrix.GetLength(0); k++)
+            {
+                resultMatrix[i,j] += firstMatrix[i,k] * secondMatrix[k,j];
+            }
+        }
+    }
+
+    return resultMatrix;
+}
