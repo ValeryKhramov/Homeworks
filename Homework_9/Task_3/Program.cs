9f5e9e09e33d7332a8f85ff2ed427b070976803c
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-02-01 09:57:22 +0300

    Added a solution to the Task_3 from Homework_9

--- Homework_9/Task_3/Program.cs
+++ Homework_9/Task_3/Program.cs
@@ -5,3 +5,16 @@
 
 using static System.Console;
 Clear();
+
+Write("Enter integer positive number M and N by space: ");
+int[] parameters = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+WriteLine(functionAckerman(parameters[0], parameters[1]));
+
+int functionAckerman(int oneNum, int twoNum)
+{
+    if(oneNum == 0) return twoNum + 1;
+    else if(oneNum > 0 && twoNum == 0) return functionAckerman(oneNum -1, 1);
+    else if(oneNum > 0 && twoNum > 0) return functionAckerman(oneNum - 1, functionAckerman(oneNum, twoNum - 1));
+    else return twoNum +1;
+}
