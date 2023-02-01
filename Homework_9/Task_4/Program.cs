72b2b81a27789361a1c268dcf8d3d15498fd8dca
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-02-01 09:57:30 +0300

    Added a solution to the Task_4 from Homework_9

---
+++ Homework_9/Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу. которая проверяет, можно ли из первой строки собрать вторую.
+//Tom Marvolo Riddle => I am Lord Voldemort
+using static System.Console;
+Clear();
+
+Write("Enter first text: ");
+string textOne = ReadLine()!.ToLower().Replace(" ", "");
+char[] firstTextToArray = textOne.ToCharArray();
+
+Write("Enter second text: ");
+string textTwo = ReadLine()!.ToLower().Replace(" ", "");
+char[] secondTextToArray = textTwo.ToCharArray();
+WriteLine($"{ComparisonString(firstTextToArray, secondTextToArray)}");
+
+bool ComparisonString(char[] firstText, char[] secondText)
+{
+    Array.Sort(firstText);
+    Array.Sort(secondText);
+    for (int i = 0; i < firstText.Length; i++)
+    {
+        
+       if(firstText[i] != secondText[i]) return false;
+       
+    }
+    return true;
+}
